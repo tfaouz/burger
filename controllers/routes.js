f149f5a701a0771f98f5922b47d9d26936981164
Thibault <ThibaultFaouz@gmail.com>
2019-06-10 17:48:32 -0400

    fixed issue

--- controllers/routes.js
+++ controllers/routes.js
@@ -6,3 +6,4 @@ router.get('/', function (req, res) {
 })
 
 module.exports = router;
+
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 console.log("server.js working fine");
 
-var PORT = process.env.PORT || 3000;
-
 var express = require("express");
 var exphbs = require('express-handlebars');
 
@@ -16,8 +14,10 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 
 app.set('view engine', 'handlebars');
 
-var routes = require('./controllers/burger_routes.js');
-app.use(routes);
+var routes = require('./controllers/routes.js');
+app.use('/', routes);
+
+var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
     console.log('Server listening on http://localhost:' + PORT);
